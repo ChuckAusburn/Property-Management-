4c99070b029500d76b86a2247029eb7e871006f1
Omar C <omar_carmona00@yahoo.com>
2018-10-16 20:13:28 -0700

    Update Request Page

--- assets/request.js
+++ assets/request.js
@@ -12,32 +12,33 @@ $(document).ready(function() {
 
     var database = firebase.database();
 
+    //Submits email, password, location, and issue(s) to firebase
+    $("#submitRequest").on("click", function(event) {
+        event.preventDefault();
+        var tenantName = $("#tenantName").val().trim();
+        console.log(tenantName);
+        var email = $("#inputEmail").val().trim();
+        console.log(email);
+        var phoneNum = $("#inputPhoneNum").val().trim();
+        console.log(phoneNum);
+        
+    });
+
+
     //Refers to the parent node("maintenance_issue") location in our database and returns a snapshot of its info
-    database.ref("/maintenance_issue").on("child_added" , function(snapshot) {
+    database.ref("/maintenance_contact").on("child_added" , function(snapshot) {
         //Logging the value of the snapshot to see what it returns
         console.log(snapshot.val());
 
-        // //Stores the value of the snapshot into html elements and displays them in the browser
-        // $("#dropDown").append(
-        //     "<option class = issues>" + snapshot.val().
-        // );
+        //Stores the value of the snapshot into html elements and displays them in the browser
+        $("#displayAddress").append(
+           "<a class='dropdown-item address' href='#'>" + snapshot.val().fb_addressInput + "</a>"
+        );
 
-        // }, // If any errors are experienced, log them to console.
-        // function(errorObject) {
-        // console.log("The read failed: " + errorObject.code);
+        }, // If any errors are experienced, log them to console.
+        function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
     });
-
-    // //Submits email, password, location, and issue(s) to firebase
-    // $("#submitRequest").on("click", function(event) {
-    //     event.preventDefault();
-    //     var email = $("#inputEmail").val().trim();
-    //     console.log(email);
-    //     var password = $("#inputPassword").val().trim();
-    //     console.log(password);
-    //     var address = 
-
-        
-    // });
 
 
 });
